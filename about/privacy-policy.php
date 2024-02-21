93fe5cfdcee8a7bcb5ecde06c739df0e215cca73
lovedeep <156370154+lovedeepgarg@users.noreply.github.com>
2024-02-20 23:27:56 -0500

    Add files via upload

---
+++ about/privacy-policy.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Terms Of Use | COMP1002 HTML, CSS, and JS Fundamentals</title>
+		<meta name="author" content="Lovedeep Garg">
+		<meta name="description" content="A concise and accurate summary of the document content is to appear here">
+		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+		<link rel="stylesheet" href="../css/styles.css" />
+	</head>
+	<body id="section6">
+		<!-- Page-level header -->
+		<header>
+			<img src="../images/html-logo.svg" height="75" width="75" alt="HTML" />
+			<h1>HTML, CSS, and JS Fundamentals</h1>
+			<h2>Building Blocks for Web Developers</h2>
+			<!-- Global site navigation -->
+			<nav>
+				<ul>
+					<li><a href="../index.php" title="Go to the Home page">Home</a></li>
+					<li><a href="../red.php" title="Learn about red blocks">Red Blocks</a></li>
+					<li><a href="../blue.php" title="Learn about blue blocks">Blue Blocks</a></li>
+					<li><a href="../yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
+					<li><a href="../green.php" title="Learn about green blocks">Green Blocks</a></li>
+					<li><a href="about.php" title="About Us">About</a></li>
+				</ul>
+			</nav>
+		</header>
+		<!-- Page-level main content -->
+		<main>
+			<section>
+				<h3>Terms Of Use</h3>
+				<p>Turpis nunc eget lorem dolor sed. Vel facilisis volutpat est velit egestas dui id. Quam quisque id diam vel quam. Mauris a diam maecenas sed enim. Tortor posuere ac ut consequat. Sed adipiscing diam donec adipiscing tristique risus nec feugiat. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Vitae elementum curabitur vitae nunc sed velit. Mollis nunc sed id semper. Diam vel quam elementum pulvinar. Ultrices gravida dictum fusce ut.</p>
+				<!-- Content omitted for brevity -->
+			</section>
+		</main>
+		<!-- Page-level footer -->
+		<footer>
+			<nav>
+				<a href="privacy-policy.php" title="Read our Privacy Policy">Privacy Policy</a> | <a href="privacy-policy.php#personal-information" title="Learn about how we treat your personal information">Use of Personal Information</a>
+				<a href="about.php" title="About Us">About</a> | <a href="about/terms-of-use.php" title="Read our Terms of Use">Terms of Use</a> | <a href="about/terms-of-use.php#limitation-liability" title="Limitation of Liability">Limitation of Liability</a>
+			
+			</nav>
+			<p><small>© HTML, CSS, and JS Blocks Inc.</small></p>
+		</footer>
+	</body>
+</html>
